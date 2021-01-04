45ed7816d66615c1b1c19ece017f34ab79266362
oufa00 <nefnafraouf@gmail.com>
2021-01-04 10:37:27 +0100

    First commit

---
+++ app/Models/Admin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Model
+{
+    use HasFactory;
+    use Notifiable;
+    protected $table='admins';
+    protected $fillable = [
+        'name',
+        'email',
+        'photo',
+        'password',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
